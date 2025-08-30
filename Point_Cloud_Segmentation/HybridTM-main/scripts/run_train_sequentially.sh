11ec57e77c0e145c953c71d81e3abd29c3506604
cugitee <2289068115@qq.com>
2025-08-31 07:30:04 +0800

    第一次修改新模型

--- Point_Cloud_Segmentation/HybridTM-main/scripts/run_train_sequentially.sh
+++ Point_Cloud_Segmentation/HybridTM-main/scripts/run_train_sequentially.sh
@@ -4,31 +4,31 @@
 echo "开始执行训练任务..."
 
 # 第一个训练任务
-echo "执行第一个训练任务: semseg-oacnns-PFEC-base"
-sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-PFEC-base -n semseg-oacnns-PFEC-base
+echo "执行第一个训练任务: semseg-oacnns-TopoFusion-ABC"
+sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-TopoFusion-ABC -n semseg-oacnns-TopoFusion-ABC
 
 # 第二个训练任务
-echo "执行第二个训练任务: semseg-oacnns-PFEC-A"
-sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-PFEC-A -n semseg-oacnns-PFEC-A
+echo "执行第二个训练任务: semseg-oacnns-TopoFusion-A"
+sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-TopoFusion-A -n semseg-oacnns-TopoFusion-A
 
 # 第三个训练任务
-echo "执行第三个训练任务: semseg-oacnns-PFEC-B"
-sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-PFEC-B -n semseg-oacnns-PFEC-B
+echo "执行第三个训练任务: semseg-oacnns-TopoFusion-B"
+sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-TopoFusion-B -n semseg-oacnns-TopoFusion-B
 
 # 第四个训练任务
-echo "执行第四个训练任务: semseg-oacnns-PFEC-C"
-sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-PFEC-C -n semseg-oacnns-PFEC-C
+echo "执行第四个训练任务: semseg-oacnns-TopoFusion-C"
+sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-TopoFusion-C -n semseg-oacnns-TopoFusion-C
 
 # 第五个训练任务
-echo "执行第五个训练任务: semseg-oacnns-PFEC-AB"
-sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-PFEC-AB -n semseg-oacnns-PFEC-AB
+echo "执行第五个训练任务: semseg-oacnns-TopoFusion-AB"
+sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-TopoFusion-AB -n semseg-oacnns-TopoFusion-AB
 
 # 第六个训练任务
-echo "执行第六个训练任务: semseg-oacnns-PFEC-AC"
-sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-PFEC-AC -n semseg-oacnns-PFEC-AC
+echo "执行第六个训练任务: semseg-oacnns-TopoFusion-AC"
+sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-TopoFusion-AC -n semseg-oacnns-TopoFusion-AC
 
 # 第七个训练任务
-echo "执行第七个训练任务: semseg-oacnns-PFEC-BC"
-sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-PFEC-BC -n semseg-oacnns-PFEC-BC
+echo "执行第七个训练任务: semseg-oacnns-TopoFusion-BC"
+sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-TopoFusion-BC -n semseg-oacnns-TopoFusion-BC
 
 echo "所有训练任务执行完毕！"
