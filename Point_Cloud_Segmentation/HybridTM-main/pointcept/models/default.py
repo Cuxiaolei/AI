d03e257ad3bff14a243337c48a9c1caccc8600a4
cugitee <2289068115@qq.com>
2025-09-07 13:35:58 +0800

    调试

--- Point_Cloud_Segmentation/HybridTM-main/pointcept/models/default.py
+++ Point_Cloud_Segmentation/HybridTM-main/pointcept/models/default.py
@@ -19,7 +19,7 @@ class DefaultSegmentor(nn.Module):
             input_dict["condition"] = input_dict["condition"][0]
         seg_logits = self.backbone(input_dict)  # 模型计算时已将曲率存入input_dict["curvatures"]
         # 提取曲率信息（模型中已处理，无曲率时为None）
-        curvatures = input_dict.get("curvatures", None)
+        curvatures = input_dict.get("original_curvature", None)
 
         # train
         if self.training:
