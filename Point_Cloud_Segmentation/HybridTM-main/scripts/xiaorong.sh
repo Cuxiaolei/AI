9ae42bf03a9d7555df0f03ee80fb3af6dd0bf4c0
cugitee <2289068115@qq.com>
2025-09-08 13:32:52 +0800

    调试

--- Point_Cloud_Segmentation/HybridTM-main/scripts/xiaorong.sh
+++ Point_Cloud_Segmentation/HybridTM-main/scripts/xiaorong.sh
@@ -9,15 +9,15 @@ echo "开始执行训练任务..."
 
 # 第二个训练任务
 echo "执行第二个训练任务: semseg-oacnns-TopoFusion-A"
-sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-TopoFusion-A -n semseg-oacnns-TopoFusion-A
 
 # 第三个训练任务
 echo "执行第三个训练任务: semseg-oacnns-TopoFusion-B"
 sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-TopoFusion-B -n semseg-oacnns-TopoFusion-B
 
+sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-TopoFusion-A -n semseg-oacnns-TopoFusion-A
 # 第四个训练任务
-echo "执行第四个训练任务: semseg-oacnns-TopoFusion-C"
-sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-TopoFusion-C -n semseg-oacnns-TopoFusion-C
+#echo "执行第四个训练任务: semseg-oacnns-TopoFusion-C"
+#sh scripts/train.sh -g 1 -d scannet -c semseg-oacnns-TopoFusion-C -n semseg-oacnns-TopoFusion-C
 
 ## 第五个训练任务
 #echo "执行第五个训练任务: semseg-oacnns-TopoFusion-AB"
