9ea992265e3455d3d1d0e48b389f152710da5af4
仝晓蕾 <2289068115@qq.com>
2026-03-23 13:14:17 +0800

    1

--- My-research/train.sh
+++ My-research/train.sh
@@ -3,22 +3,22 @@
 set -e  # 关键：任意指令失败则脚本立即退出（可选，增强安全性）
 
 echo darm 模型
-python -m src.main --configs src/configs/darm/darm_phm_T1_1.yaml
-python -m src.main --configs src/configs/darm/darm_phm_T1_5.yaml
-python -m src.main --configs src/configs/darm/darm_phm_T1_10.yaml
-python -m src.main --configs src/configs/darm/darm_phm_T1_20.yaml
-python -m src.main --configs src/configs/darm/darm_phm_T2_1.yaml
-python -m src.main --configs src/configs/darm/darm_phm_T2_5.yaml
-python -m src.main --configs src/configs/darm/darm_phm_T2_10.yaml
-python -m src.main --configs src/configs/darm/darm_phm_T2_20.yaml
-python -m src.main --configs src/configs/darm/darm_phm_T3_1.yaml
-python -m src.main --configs src/configs/darm/darm_phm_T3_5.yaml
-python -m src.main --configs src/configs/darm/darm_phm_T3_10.yaml
-python -m src.main --configs src/configs/darm/darm_phm_T3_20.yaml
-python -m src.main --configs src/configs/darm/darm_phm_T4_1.yaml
-python -m src.main --configs src/configs/darm/darm_phm_T4_5.yaml
-python -m src.main --configs src/configs/darm/darm_phm_T4_10.yaml
-python -m src.main --configs src/configs/darm/darm_phm_T4_20.yaml
+#python -m src.main --configs src/configs/darm/darm_phm_T1_1.yaml
+#python -m src.main --configs src/configs/darm/darm_phm_T1_5.yaml
+#python -m src.main --configs src/configs/darm/darm_phm_T1_10.yaml
+#python -m src.main --configs src/configs/darm/darm_phm_T1_20.yaml
+#python -m src.main --configs src/configs/darm/darm_phm_T2_1.yaml
+#python -m src.main --configs src/configs/darm/darm_phm_T2_5.yaml
+#python -m src.main --configs src/configs/darm/darm_phm_T2_10.yaml
+#python -m src.main --configs src/configs/darm/darm_phm_T2_20.yaml
+#python -m src.main --configs src/configs/darm/darm_phm_T3_1.yaml
+#python -m src.main --configs src/configs/darm/darm_phm_T3_5.yaml
+#python -m src.main --configs src/configs/darm/darm_phm_T3_10.yaml
+#python -m src.main --configs src/configs/darm/darm_phm_T3_20.yaml
+#python -m src.main --configs src/configs/darm/darm_phm_T4_1.yaml
+#python -m src.main --configs src/configs/darm/darm_phm_T4_5.yaml
+#python -m src.main --configs src/configs/darm/darm_phm_T4_10.yaml
+#python -m src.main --configs src/configs/darm/darm_phm_T4_20.yaml
 
 python -m src.main --configs src/configs/darm/darm_pu_T5_1.yaml
 python -m src.main --configs src/configs/darm/darm_pu_T5_5.yaml
