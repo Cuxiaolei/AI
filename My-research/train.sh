b2063dd2a23186d8e1bfb903ccda5d45ef2f21f8
仝晓蕾 <2289068115@qq.com>
2026-04-05 19:55:05 +0800

    1

--- My-research/train.sh
+++ My-research/train.sh
@@ -247,40 +247,40 @@ set -e  # 关键：任意指令失败则脚本立即退出（可选，增强安�
 #python -m src.main --configs src/configs/mldg/mldg_pu_T8_10.yaml
 #python -m src.main --configs src/configs/mldg/mldg_pu_T8_20.yaml
 
-echo vrex 模型
-python -m src.main --configs src/configs/vrex/vrex_phm_T1_1.yaml
-python -m src.main --configs src/configs/vrex/vrex_phm_T1_5.yaml
-python -m src.main --configs src/configs/vrex/vrex_phm_T1_10.yaml
-python -m src.main --configs src/configs/vrex/vrex_phm_T1_20.yaml
-python -m src.main --configs src/configs/vrex/vrex_phm_T2_1.yaml
-python -m src.main --configs src/configs/vrex/vrex_phm_T2_5.yaml
-python -m src.main --configs src/configs/vrex/vrex_phm_T2_10.yaml
-python -m src.main --configs src/configs/vrex/vrex_phm_T2_20.yaml
-python -m src.main --configs src/configs/vrex/vrex_phm_T3_1.yaml
-python -m src.main --configs src/configs/vrex/vrex_phm_T3_5.yaml
-python -m src.main --configs src/configs/vrex/vrex_phm_T3_10.yaml
-python -m src.main --configs src/configs/vrex/vrex_phm_T3_20.yaml
-python -m src.main --configs src/configs/vrex/vrex_phm_T4_1.yaml
-python -m src.main --configs src/configs/vrex/vrex_phm_T4_5.yaml
-python -m src.main --configs src/configs/vrex/vrex_phm_T4_10.yaml
-python -m src.main --configs src/configs/vrex/vrex_phm_T4_20.yaml
-
-python -m src.main --configs src/configs/vrex/vrex_pu_T5_1.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T5_5.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T5_10.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T5_20.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T6_1.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T6_5.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T6_10.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T6_20.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T7_1.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T7_5.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T7_10.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T7_20.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T8_1.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T8_5.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T8_10.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T8_20.yaml
+#echo vrex 模型
+#python -m src.main --configs src/configs/vrex/vrex_phm_T1_1.yaml
+#python -m src.main --configs src/configs/vrex/vrex_phm_T1_5.yaml
+#python -m src.main --configs src/configs/vrex/vrex_phm_T1_10.yaml
+#python -m src.main --configs src/configs/vrex/vrex_phm_T1_20.yaml
+#python -m src.main --configs src/configs/vrex/vrex_phm_T2_1.yaml
+#python -m src.main --configs src/configs/vrex/vrex_phm_T2_5.yaml
+#python -m src.main --configs src/configs/vrex/vrex_phm_T2_10.yaml
+#python -m src.main --configs src/configs/vrex/vrex_phm_T2_20.yaml
+#python -m src.main --configs src/configs/vrex/vrex_phm_T3_1.yaml
+#python -m src.main --configs src/configs/vrex/vrex_phm_T3_5.yaml
+#python -m src.main --configs src/configs/vrex/vrex_phm_T3_10.yaml
+#python -m src.main --configs src/configs/vrex/vrex_phm_T3_20.yaml
+#python -m src.main --configs src/configs/vrex/vrex_phm_T4_1.yaml
+#python -m src.main --configs src/configs/vrex/vrex_phm_T4_5.yaml
+#python -m src.main --configs src/configs/vrex/vrex_phm_T4_10.yaml
+#python -m src.main --configs src/configs/vrex/vrex_phm_T4_20.yaml
+#
+#python -m src.main --configs src/configs/vrex/vrex_pu_T5_1.yaml
+#python -m src.main --configs src/configs/vrex/vrex_pu_T5_5.yaml
+#python -m src.main --configs src/configs/vrex/vrex_pu_T5_10.yaml
+#python -m src.main --configs src/configs/vrex/vrex_pu_T5_20.yaml
+#python -m src.main --configs src/configs/vrex/vrex_pu_T6_1.yaml
+#python -m src.main --configs src/configs/vrex/vrex_pu_T6_5.yaml
+#python -m src.main --configs src/configs/vrex/vrex_pu_T6_10.yaml
+#python -m src.main --configs src/configs/vrex/vrex_pu_T6_20.yaml
+#python -m src.main --configs src/configs/vrex/vrex_pu_T7_1.yaml
+#python -m src.main --configs src/configs/vrex/vrex_pu_T7_5.yaml
+#python -m src.main --configs src/configs/vrex/vrex_pu_T7_10.yaml
+#python -m src.main --configs src/configs/vrex/vrex_pu_T7_20.yaml
+#python -m src.main --configs src/configs/vrex/vrex_pu_T8_1.yaml
+#python -m src.main --configs src/configs/vrex/vrex_pu_T8_5.yaml
+#python -m src.main --configs src/configs/vrex/vrex_pu_T8_10.yaml
+#python -m src.main --configs src/configs/vrex/vrex_pu_T8_20.yaml
 
 echo mcpdg 模型
 python -m src.main --configs src/configs/mcpdg/mcpdg_phm_T1_1.yaml
@@ -317,4 +317,4 @@ python -m src.main --configs src/configs/mcpdg/mcpdg_pu_T8_5.yaml
 python -m src.main --configs src/configs/mcpdg/mcpdg_pu_T8_10.yaml
 python -m src.main --configs src/configs/mcpdg/mcpdg_pu_T8_20.yaml
 
-shutdown -h now
+#shutdown -h now
