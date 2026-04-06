0c80a477906138638278ce2dfe49f1bfdcd47835
仝晓蕾 <2289068115@qq.com>
2026-04-06 14:53:49 +0800

    1

--- My-research/src/models/__init__.py
+++ My-research/src/models/__init__.py
@@ -106,7 +106,7 @@ def build_method(cfg: dict):
             # meta split
             meta_test_domains=int(model_cfg.get('meta_test_domains')),
             meta_randomize=bool(model_cfg.get('meta_randomize', True)),
-            meta_split_seed=int(model_cfg.get('meta_split_seed'), 42),
+            meta_split_seed=int(model_cfg.get('meta_split_seed')),
             meta_debug=bool(model_cfg.get('meta_debug')),
             meta_debug_max_steps = int(model_cfg.get('meta_debug_max_steps'), 20),
         ))
