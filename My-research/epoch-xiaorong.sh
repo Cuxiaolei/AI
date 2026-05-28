24a7d85f39fb3456c2bce9b31c13ece1a2959136
仝晓蕾 <2289068115@qq.com>
2026-05-28 19:00:34 +0800

    1

--- My-research/epoch-xiaorong.sh
+++ My-research/epoch-xiaorong.sh
@@ -2,25 +2,25 @@
 # 批量执行指令的脚本
 set -e  # 关键：任意指令失败则脚本立即退出（可选，增强安全性）
 
-echo "phm"
-python -m src.main --configs src/configs/mcpdgE1/mcpdgE1_phm_T2_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE2/mcpdgE2_phm_T2_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE3/mcpdgE3_phm_T2_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE4/mcpdgE4_phm_T2_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE5/mcpdgE5_phm_T2_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE6/mcpdgE6_phm_T2_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE7/mcpdgE7_phm_T2_5-1.yaml
-python -m src.main --configs src/configs/mcpdg/mcpdg_phm_T2_5-1.yaml
+#echo "phm"
+#python -m src.main --configs src/configs/mcpdgE1/mcpdgE1_phm_T2_5-1.yaml
+#python -m src.main --configs src/configs/mcpdgE2/mcpdgE2_phm_T2_5-1.yaml
+#python -m src.main --configs src/configs/mcpdgE3/mcpdgE3_phm_T2_5-1.yaml
+#python -m src.main --configs src/configs/mcpdgE4/mcpdgE4_phm_T2_5-1.yaml
+#python -m src.main --configs src/configs/mcpdgE5/mcpdgE5_phm_T2_5-1.yaml
+#python -m src.main --configs src/configs/mcpdgE6/mcpdgE6_phm_T2_5-1.yaml
+#python -m src.main --configs src/configs/mcpdgE7/mcpdgE7_phm_T2_5-1.yaml
+#python -m src.main --configs src/configs/mcpdg/mcpdg_phm_T2_5-1.yaml
 
 
 echo "pu"
-python -m src.main --configs src/configs/mcpdgE1/mcpdgE1_phm_T6_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE2/mcpdgE2_phm_T6_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE3/mcpdgE3_phm_T6_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE4/mcpdgE4_phm_T6_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE5/mcpdgE5_phm_T6_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE6/mcpdgE6_phm_T6_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE7/mcpdgE7_phm_T6_5-1.yaml
-python -m src.main --configs src/configs/mcpdg/mcpdg_phm_T6_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE1/mcpdgE1_pu_T6_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE2/mcpdgE2_pu_T6_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE3/mcpdgE3_pu_T6_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE4/mcpdgE4_pu_T6_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE5/mcpdgE5_pu_T6_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE6/mcpdgE6_pu_T6_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE7/mcpdgE7_pu_T6_5-1.yaml
+python -m src.main --configs src/configs/mcpdg/mcpdg_pu_T6_5-1.yaml
 
 
--- My-research/epoch.sh
+++ My-research/epoch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # 批量执行指令的脚本
 set -e  # 关键：任意指令失败则脚本立即退出（可选，增强安全性）
-
-echo "duibi"
-./epoch-duibi.sh
+#
+#echo "duibi"
+#./epoch-duibi.sh
 
 echo "xiaorong"
 ./epoch-xiaorong.sh
