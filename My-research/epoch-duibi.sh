ff858fa53a1454b7fe88651a7a6f80085e79e233
仝晓蕾 <2289068115@qq.com>
2026-05-28 16:04:30 +0800

    1

--- My-research/epoch-duibi.sh
+++ My-research/epoch-duibi.sh
@@ -24,21 +24,21 @@ python -m src.main --configs src/configs/mcpdg/mcpdg_phm_T2_5-1.yaml
 
 
 echo "pu"
-python -m src.main --configs src/configs/darm/darm_pu_T2_1-1.yaml
-python -m src.main --configs src/configs/darm/darm_pu_T2_5-1.yaml
-python -m src.main --configs src/configs/dfdn/dfdn_pu_T2_1-1.yaml
-python -m src.main --configs src/configs/dfdn/dfdn_pu_T2_5-1.yaml
-python -m src.main --configs src/configs/dpjdg/dpjdg_pu_T2_1-1.yaml
-python -m src.main --configs src/configs/dpjdg/dpjdg_pu_T2_5-1.yaml
-python -m src.main --configs src/configs/erm/erm_pu_T2_1-1.yaml
-python -m src.main --configs src/configs/erm/erm_pu_T2_5-1.yaml
-python -m src.main --configs src/configs/masfd/masfd_pu_T2_1-1.yaml
-python -m src.main --configs src/configs/masfd/masfd_pu_T2_5-1.yaml
-python -m src.main --configs src/configs/mldg/mldg_pu_T2_1-1.yaml
-python -m src.main --configs src/configs/mldg/mldg_pu_T2_5-1.yaml
-python -m src.main --configs src/configs/sdagn/sdagn_pu_T2_1-1.yaml
-python -m src.main --configs src/configs/sdagn/sdagn_pu_T2_5-1.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T2_1-1.yaml
-python -m src.main --configs src/configs/vrex/vrex_pu_T2_5-1.yaml
-python -m src.main --configs src/configs/mcpdg/mcpdg_pu_T2_1-1.yaml
-python -m src.main --configs src/configs/mcpdg/mcpdg_pu_T2_5-1.yaml
+python -m src.main --configs src/configs/darm/darm_pu_T6_1-1.yaml
+python -m src.main --configs src/configs/darm/darm_pu_T6_5-1.yaml
+python -m src.main --configs src/configs/dfdn/dfdn_pu_T6_1-1.yaml
+python -m src.main --configs src/configs/dfdn/dfdn_pu_T6_5-1.yaml
+python -m src.main --configs src/configs/dpjdg/dpjdg_pu_T6_1-1.yaml
+python -m src.main --configs src/configs/dpjdg/dpjdg_pu_T6_5-1.yaml
+python -m src.main --configs src/configs/erm/erm_pu_T6_1-1.yaml
+python -m src.main --configs src/configs/erm/erm_pu_T6_5-1.yaml
+python -m src.main --configs src/configs/masfd/masfd_pu_T6_1-1.yaml
+python -m src.main --configs src/configs/masfd/masfd_pu_T6_5-1.yaml
+python -m src.main --configs src/configs/mldg/mldg_pu_T6_1-1.yaml
+python -m src.main --configs src/configs/mldg/mldg_pu_T6_5-1.yaml
+python -m src.main --configs src/configs/sdagn/sdagn_pu_T6_1-1.yaml
+python -m src.main --configs src/configs/sdagn/sdagn_pu_T6_5-1.yaml
+python -m src.main --configs src/configs/vrex/vrex_pu_T6_1-1.yaml
+python -m src.main --configs src/configs/vrex/vrex_pu_T6_5-1.yaml
+python -m src.main --configs src/configs/mcpdg/mcpdg_pu_T6_1-1.yaml
+python -m src.main --configs src/configs/mcpdg/mcpdg_pu_T6_5-1.yaml
