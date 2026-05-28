b2a74fb6e2ba356be8fc36264fa9a66e900fa5fc
仝晓蕾 <2289068115@qq.com>
2026-05-28 12:11:13 +0800

    1

--- My-research/epoch-xiaorong.sh
+++ My-research/epoch-xiaorong.sh
@@ -4,23 +4,23 @@ set -e  # 关键：任意指令失败则脚本立即退出（可选，增强安�
 
 echo "phm"
 python -m src.main --configs src/configs/mcpdgE1/mcpdgE1_phm_T2_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE1/mcpdgE2_phm_T2_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE1/mcpdgE3_phm_T2_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE1/mcpdgE4_phm_T2_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE1/mcpdgE5_phm_T2_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE1/mcpdgE6_phm_T2_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE1/mcpdgE7_phm_T2_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE2/mcpdgE2_phm_T2_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE3/mcpdgE3_phm_T2_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE4/mcpdgE4_phm_T2_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE5/mcpdgE5_phm_T2_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE6/mcpdgE6_phm_T2_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE7/mcpdgE7_phm_T2_5-1.yaml
 python -m src.main --configs src/configs/mcpdg/mcpdg_phm_T2_5-1.yaml
 
 
 echo "pu"
 python -m src.main --configs src/configs/mcpdgE1/mcpdgE1_phm_T6_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE1/mcpdgE2_phm_T6_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE1/mcpdgE3_phm_T6_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE1/mcpdgE4_phm_T6_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE1/mcpdgE5_phm_T6_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE1/mcpdgE6_phm_T6_5-1.yaml
-python -m src.main --configs src/configs/mcpdgE1/mcpdgE7_phm_T6_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE2/mcpdgE2_phm_T6_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE3/mcpdgE3_phm_T6_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE4/mcpdgE4_phm_T6_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE5/mcpdgE5_phm_T6_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE6/mcpdgE6_phm_T6_5-1.yaml
+python -m src.main --configs src/configs/mcpdgE7/mcpdgE7_phm_T6_5-1.yaml
 python -m src.main --configs src/configs/mcpdg/mcpdg_phm_T6_5-1.yaml
 
 
